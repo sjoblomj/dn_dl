6a4cfaadd64e6629bec2c5b23ca68d7056dafad5
Johan Sjöblom <sjoblomj88@gmail.com>
2022-09-01 21:26:11 +0200

    Various script improvements; parameterising Latex template

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-dn_adress="https://www.dn.se/av/andrev-walden/?offset="
+dn_adress="https://www.dn.se/av/andrev-walden/"
+page_title="Andrev Walden"
+download=0 #Convenience flag - turn off to use what has already been downloaded in previous runs
 
 function download_article_list() {
   offset=0
-  while true; do
+  while [ $download -eq 1 ]; do
     echo "Downloading $offset articles"
-    curl -s --header "$(cat dn_header)" ${dn_adress}${offset} | awk 'BEGIN{in_list = 0; url = ""; found_articles = 0;}{if ($0 ~ "<div class=\"timeline-page__listing\">") { url = ""; in_list = 1; } if ($0 ~ "<div class=\"pagination") in_list = 0; if ($0 ~ "<a href" && in_list) { sub(/ *<a href="/, "", $0); sub(/" .*/, "", $0); sub(/\/$/, "", $0); url = "https://www.dn.se" $0; } if ($0 ~ "<time " && in_list) { sub(/.*="/, "", $0); sub(/T.*/, "", $0); print $0 " " url >> "article_list"; found_articles = 1;}} END{if (!found_articles) exit 1;}'
+    curl -s --header "$(cat dn_header)" "${dn_adress}?offset=${offset}" | awk 'BEGIN{in_list = 0; url = ""; found_articles = 0;}{if ($0 ~ "<div class=\"timeline-page__listing\">") { url = ""; in_list = 1; } if ($0 ~ "<div class=\"pagination") in_list = 0; if ($0 ~ "<a href" && in_list) { sub(/ *<a href="/, "", $0); sub(/" .*/, "", $0); sub(/\/$/, "", $0); url = "https://www.dn.se" $0; } if ($0 ~ "<time " && in_list) { sub(/.*="/, "", $0); sub(/T.*/, "", $0); print $0 " " url >> "article_list"; found_articles = 1;}} END{if (!found_articles) exit 1;}'
     RESULT=$?
     if [ $RESULT -ne 0 ]; then
       break
@@ -19,10 +21,10 @@ function create_latex() {
   date="${1}"
   name="${2}"
 
-  # cmark-gfm doesn't do a good job with images, do a manual override
+  # cmark-gfm doesn't do a good job with Latex images; do a hacky manual override
   sed -i -E "s/\!\[(.*)\]/@£\1¤/g" article.md
 
-  cmark-gfm -e table --table-prefer-style-attributes --to latex article.md >> "${name}.tex"
+  cmark-gfm -e table --table-prefer-style-attributes --to latex article.md > "${name}.tex"
 
   # Recreate proper images and tables
   sed -i -E "s/@£(.*)¤\(([^ ]*) ?(.*)\)/\\\\begin\{figure\}\[ht\!\]\n\\\\centering\n\\\\includegraphics\[width=0.95\\\\textwidth\]\{${date}_${name}\/\2\}\n\\\\caption\{\3 \1\}\n\\\\end\{figure\}/g" "${name}.tex"
@@ -35,25 +37,26 @@ function article_to_pdf() {
   date="${1}"
   url="${2}"
   name="${url##*/}"
-  mkdir "${date}_${name}"
+  mkdir -p "${date}_${name}"
   cd "${date}_${name}"
-  echo
   echo "${date} - ${name}"
 
-  curl -L -s --header "$(cat ../../dn_header)" "${url}" > article.html
+  if [ $download -eq 1 ]; then
+    curl -L -s --header "$(cat ../../dn_header)" "${url}" > article.html
+  fi
   ../../parser.awk article.html > article.md
 
-  while read img; do
-    imgname=$(echo "${img}" | sed 's|[/:]|_|g')
-    for i in $(seq 1 4); do
-      curl -s -L "${img}" -o "${imgname}"
-      if [ $? -eq 0 ]; then
-        break
+  if [ ! -f imgs ]; then
+    echo "No images found!"
+  else
+    while read img; do
+      imgname=$(echo "${img}" | sed 's|[/:]|_|g')
+      if [ $download -eq 1 ]; then
+        curl -s -L --retry 5 "${img}" -o "${imgname}"
+        convert "${imgname}" -quality 50% -resize 50% "${imgname}"
       fi
-      echo "Failed attempt $i to download ${img}"
-    done
-    convert "${imgname}" -quality 50% -resize 50% "${imgname}"
-  done < imgs
+    done < imgs
+  fi
 
   create_latex "${date}" "${name}"
   cd - > /dev/null
@@ -75,16 +78,19 @@ function create_pdfs_by_year() {
   for y in $(seq 1864 $(date +%Y)); do
     if ls $y-* 1> /dev/null 2>&1; then
       ls -1 $y-*/*.tex | sed 's/^/\\include{/' | sed 's/$/}/' > $y.tex
-      cat ../latex_defs.tex | sed 's/#YEAR#/'"$y"'/g' > articles_$y.tex
-      xelatex articles_$y.tex
+      cat ../latex_defs.tex | sed 's/#TITLE#/'"${page_title}"'/g' | sed 's/#YEAR#/'"$y"'/g' > articles_$y.tex
+
+      for i in $(seq 1 2); do
+        xelatex articles_$y.tex
+      done
     fi
   done
 }
 
 dir=$(pwd)
 
-#download_article_list
-#articles_to_pdf
+download_article_list
+articles_to_pdf
 cd "${dir}"
 create_pdfs_by_year
 
