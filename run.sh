f63ffde06f8b27f332cf4ff6f4462b42a492e73f
Johan Sjöblom <sjoblomj88@gmail.com>
2023-01-03 16:35:45 +0100

    Added ability to make the images slightly smaller if this reduces the article size

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 dn_adress="https://www.dn.se/av/andrev-walden/"
 page_title="Andrev Walden"
 download="n" #Convenience flag - turn off to use what has already been downloaded in previous runs
+use_smaller_images_if_better="y" #Compiles each article twice in order to see if the article requires fewer pages if its images were slightly smaller
 
 function download_article_list() {
   offset=0
@@ -18,6 +19,35 @@ function download_article_list() {
   done
 }
 
+function find_image_size() {
+  date="${1}"
+  name="${2}"
+  prevdir=$(pwd)
+  tmpdir=$(mktemp --directory)
+  cat ../../latex_defs.tex | sed 's/#TITLE#/'"${page_title}"'/g' | sed 's/#YEAR#/'"$name"'/g' | sed 's/tableofcontents/tableofcontents\n\\newpage/g' > article.tex
+  cp * "$tmpdir"
+  rm article.tex
+  cd "$tmpdir"
+
+  mkdir "$date"_"$name"
+  while read img; do
+    imgname=$(echo "${img}" | sed 's|[/:]|_|g')
+    mv "$imgname" "$date"_"$name"
+  done < imgs
+
+  xelatex article.tex 1> /dev/null
+  pages_big=$(pdfinfo article.pdf | awk '/^Pages:/ {print $2}')
+  sed -i 's/includegraphics\[width=0.95/includegraphics\[width=0.85/g' $name.tex
+  xelatex article.tex 1> /dev/null
+  pages_small=$(pdfinfo article.pdf | awk '/^Pages:/ {print $2}')
+  cd "$prevdir"
+  rm -r $tmpdir
+
+  if (( $pages_big > $pages_small )); then
+    sed -i 's/includegraphics\[width=0.95/includegraphics\[width=0.85/g' $name.tex
+  fi
+ }
+
 function create_latex() {
   date="${1}"
   name="${2}"
@@ -32,12 +62,17 @@ function create_latex() {
   sed -i -E "s/\\\\begin\{tabular\}\{l\}/\\\\centering\\\\begin\{tabular\}\{\|p\{0.8\\\\linewidth\}\|\}\\\\hline\\\\\\\\/g" "${name}.tex"
   sed -i -E "s/\\\\end\{tabular\}/\\\\hline\n\\\\end\{tabular\}/g" "${name}.tex"
   cat "${name}.tex" | awk 'BEGIN{in_head = 0;}{if ($0 ~ "\\\\section") { in_head = 1; print $0; } else if ($0 ~ "\\\\rule") { in_head = 0; print $0; } else if (in_head && $0 ~ "Av: ") print "\\begin{center}\\small{" $0 "\\\\"; else if (in_head && $0 ~ "Publicerad: ") print $0 "}\\end{center}"; else print $0;}' > tmp.tex && mv tmp.tex "${name}.tex"
+
+  if [ "$use_smaller_images_if_better" = "y" ]; then
+    find_image_size "${date}" "${name}"
+  fi
 }
 
 function article_to_pdf() {
   date="${1}"
   url="${2}"
   name="${url##*/}"
+  previous_dir=$(pwd)
   mkdir -p "${date}_${name}"
   cd "${date}_${name}"
   echo "${date} - ${name}"
@@ -63,18 +98,19 @@ function article_to_pdf() {
   fi
 
   create_latex "${date}" "${name}"
-  cd - > /dev/null
+  cd "$previous_dir"
 }
 
 function articles_to_pdf() {
   mkdir -p articles
+  prevdir=$(pwd)
   cd articles
 
   while read date url; do
     article_to_pdf "$date" "$url"
   done < ../article_list
 
-  cd -
+  cd "$prevdir"
 }
 
 function create_pdfs_by_year() {
